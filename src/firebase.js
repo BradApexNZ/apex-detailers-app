269439f5d88e68e7b965cf309591e870d78fbf0b
BradApexNZ <brad@apexdetailers.co.nz>
2026-06-26 11:22:27 +1200

    Add Firebase Analytics environment config

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -8,12 +9,20 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-const missingConfig = Object.entries(firebaseConfig)
-  .filter(([, value]) => !value)
-  .map(([key]) => key);
+const requiredConfigKeys = [
+  "apiKey",
+  "authDomain",
+  "projectId",
+  "storageBucket",
+  "messagingSenderId",
+  "appId"
+];
+
+const missingConfig = requiredConfigKeys.filter(key => !firebaseConfig[key]);
 
 if (missingConfig.length) {
   console.warn(
@@ -25,3 +34,11 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analyticsPromise = firebaseConfig.measurementId
+  ? isSupported()
+      .then(supported => (supported ? getAnalytics(app) : null))
+      .catch(error => {
+        console.warn("Firebase Analytics is not available in this environment.", error);
+        return null;
+      })
+  : Promise.resolve(null);
