f1119f1874030779a757debfcdc4769e797ffe20
BradApexNZ <brad@apexdetailers.co.nz>
2026-08-08 09:39:21 +1200

    Export launch public booking config

--- functions/entry.js
+++ functions/entry.js
@@ -3,3 +3,4 @@ export * from "./calendar-prospects.js";
 export * from "./calendar-import.js";
 export * from "./launch-v6.js";
 export * from "./calendar-settings-v6.js";
+export * from "./public-config-v6.js";
