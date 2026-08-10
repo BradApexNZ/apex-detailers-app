7af4f2588e974f261cae720eafb840e0c2170bdc
BradApexNZ <brad@apexdetailers.co.nz>
2026-08-10 23:20:43 +1200

    Route Calendar V6 through hardened live gateway

--- functions/entry.js
+++ functions/entry.js
@@ -4,7 +4,8 @@ export * from "./calendar-prospects.js";
 // calendar-import.js as well, because duplicate star exports make that name
 // ambiguous and Firebase then omits it from function discovery.
 export * from "./launch-v6.js";
-export * from "./calendar-settings-v6.js";
+export { saveGoogleCalendarSelectionV6 } from "./calendar-settings-v6.js";
+export { listGoogleCalendarsV6 } from "./calendar-gateway-v6.js";
 export * from "./calendar-save.js";
 export * from "./public-config-v6.js";
 export * from "./google-disconnect-v6.js";
